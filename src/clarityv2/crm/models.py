6858495336f248cd73762469bdca28e2b7b03fe2
Oleksandr Khomenko <homes89@ukr.net>
2017-10-12 23:29:06 +0300

    Invoice and CRM models base

---
+++ src/clarityv2/crm/models.py
@@ -0,0 +1,115 @@
+from decimal import Decimal
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from autoslug.fields import AutoSlugField
+from phonenumber_field.modelfields import PhoneNumberField
+from django_countries.fields import CountryField
+from djchoices import DjangoChoices, ChoiceItem
+
+
+class Contact(models.Model):
+
+    """
+    Contact details for a single contact.
+    """
+
+    label = models.CharField(_('label'), max_length=50)
+    name = models.CharField(_('name'), max_length=255)
+    email = models.EmailField(_('email'))
+    phone = PhoneNumberField()
+    address = models.CharField(_('address'), max_length=255, blank=True)
+    postal_code = models.CharField(_('postal code'), max_length=10, blank=True)
+    city = models.CharField(_('city'), max_length=255, blank=True)
+    country = CountryField(default=settings.DEFAULT_COUNTRY, verbose_name=_('Country'))
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+
+    class Meta:
+        verbose_name = _('contact')
+        verbose_name_plural = _('contacts')
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    name = models.CharField(_('name'), max_length=255)
+    slug = AutoSlugField(_('slug'), populate_from='name', unique=True)
+
+    # global company contact details
+    email = models.EmailField(_('email'))
+    phone = PhoneNumberField()
+    address = models.CharField(_('address'), max_length=255)
+    city = models.CharField(_('city'), max_length=255)
+    country = CountryField(default=settings.DEFAULT_COUNTRY, verbose_name=_('Country'))
+    language = models.CharField(_('language'), max_length=10, choices=settings.LANGUAGES, default='nl')
+
+    # invoicing fields
+    crn = models.CharField(_('registration number'), max_length=50, blank=True, help_text=_('KvK number'))
+    vat = models.CharField(_('VAT number'), max_length=50, blank=True)
+
+    contacts = models.ManyToManyField('Contact', blank=True)
+
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now=True)
+
+    class Meta:
+        verbose_name = _("client")
+        verbose_name_plural = _("clients")
+
+    def __str__(self):
+        return self.name
+
+
+class TaxRates(DjangoChoices):
+    low = ChoiceItem(Decimal('0.06'), _('low'))
+    high = ChoiceItem(Decimal('0.21'), _('high'))
+
+
+class Project(models.Model):
+
+    """
+    Logical entity to group work entities under.
+
+    Clients can have multiple projects going on.
+    """
+
+    client = models.ForeignKey(Client)
+    name = models.CharField(max_length=50)
+    slug = AutoSlugField(populate_from='name')
+
+    # base financial information
+    base_rate = models.DecimalField(_('hourly base rate'), max_digits=8, decimal_places=2, null=True, blank=True)
+    flat_fee = models.DecimalField(_('flat fee'), max_digits=10, decimal_places=2, null=True, blank=True)
+    tax_rate = models.DecimalField(
+        _('tax rate'), max_digits=4, decimal_places=2,
+        choices=TaxRates.choices, default=TaxRates.high
+    )
+
+    class Meta:
+        verbose_name = _('project')
+        verbose_name_plural = _('projects')
+        unique_together = (
+            ('client', 'slug'),
+        )
+
+    def __str__(self):
+        return '{client} - {name}'.format(name=self.name, client=self.client)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        """
+        Validate that at least one rate is provided.
+        """
+        if not self.base_rate and not self.flat_fee:
+            raise ValidationError(_('Provide a base hourly rate or a flat fee.'))
---
+++ src/clarityv2/invoices/models.py
@@ -0,0 +1,166 @@
+import logging
+import re
+from datetime import datetime, time, timedelta
+
+from django.conf import settings
+from django.core import validators
+from django.core.urlresolvers import reverse
+from django.db import models, transaction
+from django.db.models import F, Sum, Max
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils import timezone
+from django.utils.functional import cached_property
+from django.utils.translation import ugettext_lazy as _
+
+from clarityv2.crm.models import TaxRates
+# from regex.utils.storages import private_media_storage
+# from regex.work_entries.models import WorkEntry
+
+
+logger = logging.getLogger(__name__)
+
+
+RE_INVOICE_NUMBER = re.compile(r'(?P<year>20\d{2})\d{5}$')
+
+
+class Invoice(models.Model):
+    client = models.ForeignKey('crm.Client')
+    date = models.DateField(_('date'), help_text=_('Include work up to (including) this day.'))
+
+    generated = models.DateTimeField(editable=False, null=True)
+    invoice_number = models.CharField(
+        _('invoice number'), max_length=50, blank=True,
+        unique=True, default=None, null=True,
+        validators=[validators.RegexValidator(RE_INVOICE_NUMBER)]
+    )
+    due_date = models.DateTimeField(_('due date'), null=True, blank=True)
+    pdf = models.FileField(
+        _('pdf'), blank=True, upload_to='invoices/%Y/%m',
+        storage=private_media_storage
+    )
+
+    received = models.DateTimeField(_('received'), null=True, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return '{client} - {date}'.format(client=self.client, date=self.date)
+
+    def get_absolute_url(self):
+        return reverse('invoices:detail', kwargs={'invoice_number': self.invoice_number})
+
+    def get_previous(self, **kwargs):
+        kwargs['client'] = self.client
+        return self.get_previous_by_date(**kwargs)
+
+    def generate_invoice_number(self, save=True):
+        """
+        Invoice numbers must increment according to the Dutch legislation.
+
+        We choose here to prefix the year that the invoice object was created,
+        and number incrementingly (+1) across the years.
+        """
+        prefix = self.created.year
+        agg = self.__class__.objects.aggregate(Max('invoice_number'))
+        max_number = agg['invoice_number__max'] or '201500000'
+        match = RE_INVOICE_NUMBER.match(max_number)
+        if not match:
+            raise ValueError('Invalid invoice number for invoice %d', self.pk)
+        max_number = int(max_number[4:])
+        next_number = '{:05d}'.format(max_number + 1)
+        self.invoice_number = '{prefix}{number}'.format(prefix=prefix, number=next_number)
+        if save:
+            self.save()
+
+    def generate(self):
+        if self.generated is not None:
+            return
+
+        # collect the work entries
+        try:
+            previous = self.get_previous()
+            lower = datetime.combine(previous.date, time(0, 0)) + timedelta(days=1)
+        except self.__class__.DoesNotExist:
+            previous = None
+            lower = datetime(1970, 1, 1, 0, 0)
+        lower = timezone.make_aware(lower)
+        upper = timezone.make_aware(datetime.combine(self.date, time(23, 59, 59)))
+
+        work_entries = WorkEntry.objects.filter(
+            project__client=self.client, start__range=[lower, upper]
+        ).select_related('project')
+
+        with transaction.atomic():
+            for entry in work_entries:
+                InvoiceItem.objects.create(
+                    invoice=self,
+                    project=entry.project,
+                    rate=entry.project.base_rate if not entry.project.flat_fee else 0,
+                    amount=entry.delta_to_decimal(),
+                    tax_rate=entry.project.tax_rate,
+                    source_object=entry,
+                    remarks=entry.notes
+                )
+
+            # either created from hourly rate (work_entries) or manual invoice items
+            if work_entries or self.invoiceitem_set.exists():
+                self.generated = timezone.now()
+                self.generate_invoice_number(save=False)
+                self.save()
+
+    def regenerate(self):
+        if self.received is not None:
+            logger.info('Not regenerating paid invoice %d, fix this manually', self.pk)
+            return
+
+        if self.generated is not None:
+            self.invoiceitem_set.all().delete()
+            self.generated = None
+            logger.info('Regenerating invoice %d, potentially rewriting sent-out invoices.', self.pk)
+        self.generate()
+
+    def get_totals(self):
+        totals = self.invoiceitem_set.annotate(
+            base=F('rate')*F('amount'),
+            tax=F('rate')*F('amount')*F('tax_rate')
+        ).aggregate(Sum('base'), Sum('tax'))
+        return totals
+
+    @property
+    def total_no_vat(self):
+        return self.get_totals()['base__sum']
+
+    @property
+    def total_vat(self):
+        return self.get_totals()['tax__sum']
+
+    @property
+    def total_with_vat(self):
+        totals = self.get_totals()
+        return totals['base__sum'] + totals['tax__sum']
+
+    @cached_property
+    def vat_reverse_charge(self):
+        return self.client.country != settings.SITE_COUNTRY
+
+
+class InvoiceItem(models.Model):
+    invoice = models.ForeignKey(Invoice)
+    project = models.ForeignKey('crm.Project', null=True, blank=True)
+
+    rate = models.DecimalField(_('rate'), max_digits=10, decimal_places=2)
+    amount = models.DecimalField(_('amount'), max_digits=10, decimal_places=2)
+    tax_rate = models.DecimalField(
+        _('tax rate'), max_digits=4, decimal_places=2,
+        choices=TaxRates.choices, default=TaxRates.high
+    )
+
+    content_type = models.ForeignKey('contenttypes.ContentType', blank=True, null=True)
+    object_id = models.IntegerField(blank=True, null=True)
+    source_object = GenericForeignKey()
+    remarks = models.TextField(_('remarks'), blank=True)
+
+    class Meta:
+        verbose_name = _('invoice item')
+        verbose_name_plural = _('invoice items')
