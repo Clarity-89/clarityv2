6586d64e45129a2345fc565c7f5d992099ec3b34
Oleksandr Khomenko <homes89@ukr.net>
2017-10-16 20:26:42 +0300

    Add files

--- src/clarityv2/invoices/admin.py
+++ src/clarityv2/invoices/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from django.conf.urls import url
+from django.core.urlresolvers import reverse
+from django.db.models import Count
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from regex.utils.views.private_media import PrivateMediaView
+
+from .forms import AdminInvoiceForm
+from .models import Invoice, InvoiceItem
+from .utils import render_invoice_pdf
+
+
+def generate_invoices(modeladmin, request, queryset):
+    for invoice in queryset:
+        invoice.generate()
+generate_invoices.short_description = _("Generate invoice details")
+
+
+def render_pdf(modeladmin, request, queryset):
+    for invoice in queryset:
+        render_invoice_pdf(request, invoice)
+render_pdf.short_description = _("Generate invoice pdfs")
+
+
+class InvoiceItemInline(admin.TabularInline):
+    model = InvoiceItem
+    extra = 0
+
+
+class InvoicePrivateMediaView(PrivateMediaView):
+    model = Invoice
+    permission_required = 'invoices.can_view_invoice'
+    file_field = 'pdf'
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    form = AdminInvoiceForm
+    list_display = ('client', 'date', 'due_date', 'received', 'invoice_number',
+                    'created', 'n_items', 'generated', 'invoice_items')
+    list_filter = ('client', 'date', 'due_date', 'received')
+    search_fields = ('invoice_number',)
+    inlines = [InvoiceItemInline]
+    actions = [generate_invoices, render_pdf]
+
+    def get_queryset(self, request=None):
+        return super().get_queryset(request=request).annotate(n_items=Count('invoiceitem'))
+
+    def n_items(self, obj):
+        return obj.n_items
+    n_items.short_description = _('# invoice items')
+
+    def invoice_items(self, obj):
+        url = reverse('admin:invoices_invoiceitem_changelist')
+        return '<a href="{}?invoice={}">{}</a>'.format(url, obj.pk, _('invoice items'))
+    invoice_items.short_description = _('invoice items')
+    invoice_items.allow_tags = True
+
+    def get_urls(self):
+        extra = [
+            url(
+                r'^(?P<pk>.*)/pdf/$',
+                self.admin_site.admin_view(InvoicePrivateMediaView.as_view()),
+                name='invoices_invoice_pdf'
+            ),
+        ]
+        return extra + super(InvoiceAdmin, self).get_urls()
+
+
+@admin.register(InvoiceItem)
+class InvoiceItemAdmin(admin.ModelAdmin):
+    list_display = ('invoice', 'project', 'rate', 'amount', 'tax_rate')
+    list_filter = ('invoice__client', 'project')
---
+++ src/clarityv2/invoices/utils.py
@@ -0,0 +1,82 @@
+"""
+Utility functions for invoices.
+"""
+from io import BytesIO
+
+from django.core.files import File
+from django.db.models import Count
+from django.template import loader
+from django.utils import translation
+
+import weasyprint
+from sendfile import sendfile
+
+from regex.utils.pdf import UrlFetcher
+from regex.utils.views.pdf import PDFTemplateResponse, PDFTemplateResponseMixin
+
+
+def render_invoice_pdf(request, invoice, template_name='invoices/invoice_detail.html'):
+    # render the invoice in the client's language
+    lang_code = invoice.client.language
+    translation.activate(lang_code)
+
+    if isinstance(template_name, (list, tuple)):
+        template = loader.select_template(template_name)
+    else:
+        template = loader.get_template(template_name)
+
+    tax_rates = invoice.invoiceitem_set.values('tax_rate').annotate(num=Count('tax_rate'))
+    context = {
+        'invoice': invoice,
+        'tax_rates': tax_rates,
+        'items': invoice.invoiceitem_set.select_related('project').order_by('project', 'tax_rate'),
+        'request': request,
+    }
+    html = template.render(context)
+    base_url = request.build_absolute_uri("/")
+    url_fetcher = UrlFetcher(request, base_url)
+    wp = weasyprint.HTML(string=html, base_url=base_url, url_fetcher=url_fetcher)
+    # pdf as bytes
+    buffer = BytesIO()
+    wp.write_pdf(target=buffer)
+    filename = '{}.pdf'.format(invoice.invoice_number)
+    invoice.pdf.save(filename, File(buffer))
+
+    # go back to the previous language
+    if hasattr(request, 'LANGUAGE_CODE'):
+        translation.activate(request.LANGUAGE_CODE)
+    else:
+        translation.deactivate()
+
+
+class InvoicePDFTemplateResponse(PDFTemplateResponse):
+
+    def __init__(self, invoice=None, *args, **kwargs):
+        self.invoice = invoice
+        super(InvoicePDFTemplateResponse, self).__init__(*args, **kwargs)
+
+    @property
+    def rendered_content(self):
+        """
+        Read the invoices pdf, or generate it and return the rendered pdf.
+        """
+        assert self.invoice is not None
+
+        if not self.invoice.pdf:
+            render_invoice_pdf(self._request, self.invoice, template_name=self.template_name)
+        return self.invoice.pdf.read()
+
+
+class InvoicePDFTemplateResponseMixin(PDFTemplateResponseMixin):
+
+    response_class = InvoicePDFTemplateResponse
+
+    def render_to_response(self, *args, **kwargs):
+        # if the pdf exists, use sendfile
+        if self.object.pdf:
+            return sendfile(
+                self.request, self.object.pdf.path,
+                attachment=True, attachment_filename=self.get_filename()
+            )
+        kwargs['invoice'] = self.object
+        return super(InvoicePDFTemplateResponseMixin, self).render_to_response(*args, **kwargs)
