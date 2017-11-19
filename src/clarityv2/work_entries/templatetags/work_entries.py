70257135d24474dda03147b37c33d2733c7da80b
Oleksandr Khomenko <homes89@ukr.net>
2017-11-19 19:50:55 +0200

    Display total price per project

--- src/clarityv2/work_entries/templatetags/work_entries.py
+++ src/clarityv2/work_entries/templatetags/work_entries.py
@@ -1,4 +1,8 @@
+from decimal import Decimal
+
 from django import template
+
+from clarityv2.invoices.constants import TWO_DIGITS
 
 register = template.Library()
 
@@ -9,3 +13,12 @@ def duration(td):
     hours = total_seconds / 3600
 
     return '{}'.format(hours)
+
+
+@register.simple_tag
+def total_price(project, total_hours):
+    if project.flat_fee:
+        return project.flat_fee
+    rate = project.base_rate
+
+    return (Decimal(total_hours) * rate).quantize(TWO_DIGITS)
