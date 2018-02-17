d9814f30d1fe03ce4b54af8767c8b5abec2c5be0
Oleksandr Khomenko <homes89@ukr.net>
2018-02-17 23:30:13 +0200

    Fix settings

--- src/manage.py
+++ src/manage.py
@@ -4,11 +4,11 @@ import sys
 
 if __name__ == "__main__":
     if os.getenv('ON_HEROKU') == '1':
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adhoc.conf.settings.staging")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clarityv2.conf.settings.staging")
     elif os.getenv('PROD') == 'true':
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adhoc.conf.settings.production")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clarityv2.conf.settings.production")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adhoc.conf.settings.dev")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clarityv2.conf.settings.dev")
 
     from django.core.management import execute_from_command_line
 
