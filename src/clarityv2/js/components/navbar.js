e6f61a1bc06692b8f15eb7f85de53d2c4e607033
Oleksandr Khomenko <homes89@ukr.net>
2018-03-20 21:39:50 +0200

    Mobile navbar icon

--- src/clarityv2/js/components/navbar.js
+++ src/clarityv2/js/components/navbar.js
@@ -5,10 +5,12 @@ export default class Navbar {
     static init() {
         let nav = document.querySelector('.navbar__collapse');
         let navToggle = document.querySelector('.navbar__toggle');
+        let navIcon = document.getElementById('nav-icon');
 
         navToggle.addEventListener('click', () => {
             let isCollapsed = nav.classList.contains('navbar__collapse--collapsed');
             nav.classList.toggle('navbar__collapse--collapsed', !isCollapsed);
+            navIcon.classList.toggle('open');
         })
     }
 }
