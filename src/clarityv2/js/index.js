34e6bfae7356ca529e25221da7e98d3e1f1cac84
Oleksandr Khomenko <homes89@ukr.net>
2019-03-11 22:26:58 +0200

    Remove css stuff from webpack

--- src/clarityv2/js/index.js
+++ src/clarityv2/js/index.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Test} from './components/react/HelloWorld';
-import Navbar from './components/navbar';
-import './views/index';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Test } from "./components/react/HelloWorld";
+import Navbar from "./components/navbar";
+import "./views/index";
 
-// SCSS
-import '../sass/screen.scss';
-import '../sass/style.scss';
-import '../sass/print.scss';
-
-window.addEventListener('DOMContentLoaded', () => {
-    let el = document.getElementById('react-test');
+window.addEventListener("DOMContentLoaded", () => {
+    let el = document.getElementById("react-test");
     if (el) {
-        ReactDOM.render(<Test/>, el);
+        ReactDOM.render(<Test />, el);
     }
 
     Navbar.init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,48 @@
-var webpack = require('webpack');
-var paths = require('./build/paths');
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var webpack = require("webpack");
+var paths = require("./build/paths");
 
-var extractMainCss = new MiniCssExtractPlugin({
-    filename: '../css/screen.css'
-});
-
-
-var extractPrintCss = new MiniCssExtractPlugin({
-    filename: '../css/print.css'
-});
-
-// Separate loader for styles with Bootstrap
-var extractBSCss = new MiniCssExtractPlugin({
-    filename: '../css/style.css'
-});
-
-var extractConfig = {
-    fallback: 'style-loader',
-    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-};
 /**
  * Webpack configuration
  * Run using "webpack"
  */
 module.exports = {
     // Path to the js entry point (source)
-    entry: __dirname + '/' + paths.jsEntry,
+    entry: __dirname + "/" + paths.jsEntry,
 
     // Path to the (transpiled) js
     output: {
-        path: __dirname + '/' + paths.jsDir, // directory
-        filename: 'app.js', // file
+        path: __dirname + "/" + paths.jsDir, // directory
+        filename: "app.js" // file
     },
 
     module: {
         rules: [
             {
-                test: /print\.(css|sass|scss)$/,
-                use: extractConfig.use
-            }, {
-                test: /screen\.(css|sass|scss)$/,
-                use: extractConfig.use
-            }, {
-                test: /style\.(css|sass|scss)$/,
-                use: extractConfig.use
-            },
-            {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
+                use: ["file-loader"]
             },
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env'],
-                        cacheDirectory: true,
+                        presets: ["@babel/preset-env"],
+                        cacheDirectory: true
                     }
                 }
             },
             {
                 test: /\.(woff|woff2|eot|ttf)$/,
-                loader: 'url-loader'
+                loader: "url-loader"
             }
         ]
     },
 
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: '../css/[name].css'
-        })
-    ],
-
-    devtool: 'inline-source-map',
+    devtool: "inline-source-map",
 
     // Minify output
     optimization: {
         minimize: false
-    },
+    }
 };
