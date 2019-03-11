f8b1f7c4670384ad2a1523318729e134e609a47b
Oleksandr Khomenko <homes89@ukr.net>
2019-03-11 22:51:05 +0200

    Codeformat

--- src/clarityv2/js/components/navbar.js
+++ src/clarityv2/js/components/navbar.js
@@ -3,17 +3,21 @@
  */
 export default class Navbar {
     static init() {
-        let nav = document.querySelector('.navbar__collapse');
-        let navToggle = document.querySelector('.navbar__toggle');
-        let navIcon = document.getElementById('nav-icon');
+        let nav = document.querySelector(".navbar__collapse");
+        let navToggle = document.querySelector(".navbar__toggle");
+        let navIcon = document.getElementById("nav-icon");
 
         if (navToggle) {
-
-            navToggle.addEventListener('click', () => {
-                let isCollapsed = nav.classList.contains('navbar__collapse--collapsed');
-                nav.classList.toggle('navbar__collapse--collapsed', !isCollapsed);
-                navIcon.classList.toggle('open');
-            })
+            navToggle.addEventListener("click", () => {
+                let isCollapsed = nav.classList.contains(
+                    "navbar__collapse--collapsed"
+                );
+                nav.classList.toggle(
+                    "navbar__collapse--collapsed",
+                    !isCollapsed
+                );
+                navIcon.classList.toggle("open");
+            });
         }
     }
 }
