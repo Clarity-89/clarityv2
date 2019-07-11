67f0cb0e5b88fa264b004396dabf285ec452d468
Oleksandr Khomenko <homes89@ukr.net>
2019-07-11 21:15:31 +0300

    Add htmls

--- src/clarityv2/blog/views.py
+++ src/clarityv2/blog/views.py
@@ -5,14 +5,14 @@ from .models import BlogPost
 
 class BlogPostListView(ListView):
     model = BlogPost
-    queryset = BlogPost.objects.published
-    template_name = 'blog_post/list.html'
+    queryset = BlogPost.objects.published()
+    template_name = 'blog/list.html'
     context_object_name = 'blog_post'
     paginate_by = 10
 
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
-    queryset = BlogPost.objects.published
-    template_name = 'blog_post/detail.html'
+    queryset = BlogPost.objects.published()
+    template_name = 'blog/detail.html'
     context_object_name = 'blog_post'
