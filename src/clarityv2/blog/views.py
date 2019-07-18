323a91df7e014a0f6ab35cd055696afefc5f0514
Oleksandr Khomenko <homes89@ukr.net>
2019-07-18 22:52:16 +0300

    Show drafts for admin user

--- src/clarityv2/blog/views.py
+++ src/clarityv2/blog/views.py
@@ -5,10 +5,16 @@ from .models import BlogPost
 
 class BlogPostListView(ListView):
     model = BlogPost
-    queryset = BlogPost.objects.published()
+    queryset = BlogPost.objects.all()
     template_name = 'blog/list.html'
     context_object_name = 'blog_posts'
     paginate_by = 15
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.is_superuser:
+            return qs
+        return qs.published()
 
 
 class BlogPostDetailView(DetailView):
