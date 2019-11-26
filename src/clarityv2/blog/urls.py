f67cd4d0b6d826b0da43fbe1ae1a20a41652f6c9
Alex Khomenko <alex.khomenko@getuniverse.com>
2019-11-26 16:56:29 +0200

    Add trailing slash to post detail urls

--- src/clarityv2/blog/urls.py
+++ src/clarityv2/blog/urls.py
@@ -4,5 +4,5 @@ from .views import BlogPostDetailView, BlogPostListView
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='list'),
-    path('<slug>', BlogPostDetailView.as_view(), name='detail'),
+    path('<slug>/', BlogPostDetailView.as_view(), name='detail'),
 ]
