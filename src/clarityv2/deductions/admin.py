18b31402e36dc2385b4ee3c3d05351d872c64553
Oleksandr Khomenko <homes89@ukr.net>
2019-12-14 16:32:12 +0200

    Add deductions to admin

--- src/clarityv2/deductions/admin.py
+++ src/clarityv2/deductions/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.db.models import Sum
 
-# Register your models here.
+from .models import Deduction
+
+
+@admin.register(Deduction)
+class DeductionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'amount')
+    search_fields = ('name', 'notes')
+    change_list_template = 'admin/deductions/deduction/change_list.html'
+
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=None)
+
+        if hasattr(response, 'context_data'):
+            cl = response.context_data.get('cl')
+            if cl:
+                queryset = cl.get_queryset(request)
+                amount = queryset.aggregate(Sum('amount'))['amount__sum']
+                response.context_data['total_amount'] = amount
+        return response
