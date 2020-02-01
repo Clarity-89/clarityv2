98c9e97eacf2b1a9e11a58716647f1b0716bd722
Clarity-89 <homes89@ukr.net>
2020-02-01 14:24:08 +0200

    Cleanup

--- src/clarityv2/js/index.js
+++ src/clarityv2/js/index.js
@@ -1,16 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Test } from "./components/react/HelloWorld";
 import Navbar from "./components/navbar";
-import "./views/index";
 import { initHighlight } from "./components/codeHighlight";
 
 window.addEventListener("DOMContentLoaded", () => {
-    let el = document.getElementById("react-test");
-    if (el) {
-        ReactDOM.render(<Test />, el);
-    }
-
     Navbar.init();
     initHighlight();
 });
