595ef9d0c3cf333ea9934ff987fdecbb737ecf74
Clarity-89 <homes89@ukr.net>
2020-05-08 14:06:59 +0300

    Extend start script

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -6,3 +6,33 @@ set -ex
 # See: https://docs.docker.com/compose/startup-order/
 export PGHOST=${DB_HOST:-db}
 export PGPORT=${DB_PORT:-5432}
+
+uwsgi_port=${UWSGI_PORT:-8000}
+uwsgi_processes=${UWSGI_PROCESSES:-2}
+uwsgi_threads=${UWSGI_THREADS:-2}
+mountpoint=${SUBPATH:-/}
+until pg_isready; do
+  >&2 echo "Waiting for database connection..."
+  sleep 1
+done
+
+>&2 echo "Database is up."
+
+# Apply database migrations
+>&2 echo "Apply database migrations"
+python src/manage.py migrate
+
+# Start server
+>&2 echo "Starting server"
+uwsgi \
+    --http :$uwsgi_port \
+    --http-keepalive \
+    --manage-script-name \
+    --mount $mountpoint=clarity2.wsgi:application \
+    --static-map /static=/app/static \
+    --static-map /media=/app/media  \
+    --chdir src \
+    --enable-threads \
+    --processes $uwsgi_processes \
+    --threads $uwsgi_threads \
+    --buffer-size=65535
