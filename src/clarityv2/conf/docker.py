b96a4599b9e71b2e546258f12dae28ba833ae61e
Oleksandr Khomenko <homes89@ukr.net>
2020-05-14 17:27:35 +0300

    Add dotenv

---
+++ src/clarityv2/conf/docker.py
@@ -0,0 +1,18 @@
+import os
+
+
+os.environ.setdefault("DB_HOST", "db")
+os.environ.setdefault("DB_NAME", "clarity")
+os.environ.setdefault("DB_USER", "clarity")
+os.environ.setdefault("DB_PASSWORD", "clarity")
+
+os.environ.setdefault("LOG_STDOUT", "yes")
+
+from .dev import *  # noqa isort:skip
+
+
+#
+# Custom settings
+#
+
+ENVIRONMENT = "docker"
