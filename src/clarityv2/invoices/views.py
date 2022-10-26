bd3215441017a13ecca5f14025afa11ecfdd98bb
Clarity-89 <homes89@ukr.net>
2022-10-26 15:24:08 +0300

    Rebuild deps with python 3.9

--- src/clarityv2/invoices/views.py
+++ src/clarityv2/invoices/views.py
@@ -1,19 +1,21 @@
 from django.db.models import Count, Sum
 from django.views.generic import DetailView
-
-from rules.contrib.views import PermissionRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 from .models import Invoice
 from .utils import InvoicePDFTemplateResponseMixin
 
 
-class InvoiceDetailView(PermissionRequiredMixin, DetailView):
+class InvoiceDetailView(UserPassesTestMixin, DetailView):
     model = Invoice
     queryset = Invoice.objects.select_related('client').annotate(n_items=Count('invoiceitem'))
     slug_field = 'invoice_number'
     slug_url_kwarg = 'invoice_number'
     context_object_name = 'invoice'
     permission_required = 'invoices.can_view_invoice'
+
+    def test_func(self):
+        return self.request.user.is_superuser
 
     def get_context_data(self, **kwargs):
         context = super(InvoiceDetailView, self).get_context_data(**kwargs)
