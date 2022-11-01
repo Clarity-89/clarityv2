7666a02dde4faf59bebd99d89122bbe66631ddda
Clarity-89 <homes89@ukr.net>
2022-11-01 15:59:37 +0200

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require('node-sass'));
 const paths = require("./build/paths");
 const sourcemaps = require("gulp-sourcemaps");
 const del = require("del");
