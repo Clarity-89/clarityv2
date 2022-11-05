17f0132a8e5dc970b89cc891ce56975f0fa22c88
Clarity-89 <homes89@ukr.net>
2022-11-05 11:03:42 +0200

    Fix env path

--- src/manage.py
+++ src/manage.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
     # load the environment variables containing the secrets/config
-    dotenv_path = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, ".env")
+    dotenv_path = os.path.join(os.path.dirname(__file__), os.pardir, ".env")
     load_dotenv(dotenv_path)
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clarityv2.conf.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv('DJANGO_SETTINGS_MODULE', "clarityv2.conf.dev"))
 
     from django.core.management import execute_from_command_line
 
